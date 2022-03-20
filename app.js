5da1cadca9f29d58c834ed995d89fb7cc8c40df0
Jibirin-Yaro <yazeed.jibirin-yaro@Terra-103.local>
2022-03-20 12:33:27 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require ('express');
+const app = express();
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+require('dotenv/config');
+const cors = require('cors')
+
+
+//Middleware
+app.use(cors())
+app.options('*', cors())
+
+app.use(express.json());
+app.use(morgan('tiny'))
+
+
+//Routes
+const productRoutes = require('./routes/products')
+const categoriesRoutes = require('./routes/categories')
+const ordersRoutes = require('./routes/orders')
+const usersRoutes = require('./routes/users')
+
+
+
+const api = process.env.API_URL;
+
+
+app.use(`${api}/products`, productRoutes)
+app.use(`${api}/categories`, categoriesRoutes)
+app.use(`${api}/orders`, ordersRoutes)
+app.use(`${api}/users`, usersRoutes)
+
+
+
+//Database Connection
+mongoose.connect(process.env.DB_CONNECTION_STRING)
+.then(() => {
+    console.log('Connection is ready!')
+})
+.catch((err) => {
+    console.log(err)
+})
+
+//Server
+app.listen(3000, () =>{
+    console.log('Serer is running on http://localhost:3000');
+} )
---
+++ routes/categories.js
@@ -0,0 +1,57 @@
+const {Category} = require('../models/category')
+const express = require('express')
+const router = express.Router();
+
+
+router.get(`/`, async (req, res) => {
+    const categoryList = await Category.find();
+
+    if(!categoryList){
+        res.status(500).json({success: false})
+    }
+    res.status(200).send(categoryList)
+})
+
+router.get('/:id', async(req, res) => {
+    const category = await Category.findById(req.params.id)
+
+    if(!category){
+        res.status(500).json({message: 'No category with given ID.'})
+    }
+    res.status(200).send(category)
+})
+
+router.post('/', async (req, res) => {
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color
+    })
+    category = await category.save();
+
+    if(!category){
+    return res.status(404).send('The Category cannot be created')
+    }
+    res.send(category)
+})
+
+router.delete('/:id', (req, res) => {
+    Category.findByIdAndDelete(req.params.id).then(category => {
+        if(category){
+            return res.status(200).json({
+                success: true,
+                message: 'The category has been deleted'
+            })
+        } else{
+            return res.status(404).json({
+                success: false,
+                message: 'Category not found'
+            })
+        }
+    }).catch(err => {
+        return res .status(400).json({ success: false, error: err})
+    })
+})
+
+
+module.exports = router;
