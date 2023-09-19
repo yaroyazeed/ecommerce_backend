8f481cac8a62ac5cd60d52f0e07af5b07a7bc8a1
Jibirin-Yaro <yazeed.jibirin-yaro@Terra-103.local>
2023-09-19 11:57:10 +0100

    stashing updates

--- models/user.js
+++ models/user.js
@@ -1,12 +1,59 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true 
-    }
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    street: {
+        type: String,
+        default: '',
+    },
+    apartment: {
+        type: String,
+        default: '',
+    },
+    zip: {
+        type: String,
+        default: '',
+    },
+    city: {
+        type: String,
+        default: '',
+    },
+    country: {
+        type: String,
+        default: '',
+    },
+    // image: {
+    //     type: String,
+    //     required: true
+    // },
+})
+
+userSchema.virtual('id').get(function () {
+    return this._id.toHexString()
+})
+
+userSchema.set('toJson', {
+    virtuals: true,
 })
 
 exports.User = mongoose.model('User', userSchema)
+exports.userSchema = userSchema
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,128 @@
-const User = require('../models/user')
+const { User } = require('../models/user')
 const express = require('express')
-const router = express.Router();
-
+const router = express.Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { token } = require('morgan')
 
 router.get(`/`, async (req, res) => {
-    const userList = await User.find();
+    const userList = await User.find().select('-passwordHash')
 
-    if(!userList){
-        res.status(500).json({success: false})
+    if (!userList) {
+        res.status(500).json({ success: false })
     }
     res.send(userList)
 })
 
+//GET single user
+router.get('/:id', async (req, res) => {
+    //.select() choose which fields to display .select('-') remove fields from result
+    const user = await User.findById(req.params.id).select('-passwordHash')
 
-module.exports = router;
+    if (!user) {
+        res.status(500).json({ message: 'No user with given ID.' })
+    }
+    res.status(200).send(user)
+})
+
+router.post('/', async (req, res) => {
+    var user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.appartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save()
+
+    if (!user) {
+        return res.status(404).send('The user cannot be created or registered')
+    }
+    res.send(user)
+})
+
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({ email: req.body.email })
+    const secret = process.env.secret
+
+    if (!user) {
+        return res.status(400).send('User not found')
+    }
+
+    if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+        const token = jwt.sign(
+            {
+                userID: user.id,
+                isAdmin: user.isAdmin,
+            },
+            secret,
+            { expiresIn: '1d' }
+        )
+
+        return res.status(200).send({ user: user.email, token: token })
+    } else {
+        res.status(400).send('Password is wrong')
+    }
+
+    // return res.status(200).send(user)
+})
+
+router.post('/register', async (req, res) => {
+    var user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.appartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save()
+
+    if (!user) {
+        return res.status(404).send('The user cannot be created')
+    }
+    res.send(user)
+})
+
+//GET count
+router.get(`/get/count`, async (req, res) => {
+    const userCount = await User.countDocuments()
+
+    if (!userCount) {
+        res.status(500).json({ success: false })
+    }
+    res.send({ userCount: userCount })
+})
+
+//DELETE user
+router.delete('/:id', (req, res) => {
+    User.findByIdAndDelete(req.params.id)
+        .then((user) => {
+            if (user) {
+                return res.status(200).json({
+                    success: true,
+                    message: 'The user has been deleted',
+                })
+            } else {
+                return res.status(404).json({
+                    success: false,
+                    message: 'User not found',
+                })
+            }
+        })
+        .catch((err) => {
+            return res.status(400).json({ success: false, error: err })
+        })
+})
+
+module.exports = router
