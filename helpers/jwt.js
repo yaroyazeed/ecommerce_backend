3e660eabbe32e18b9ef77423a320fa96ea644a49
Jibirin-Yaro <yazeed.jibirin-yaro@Terra-103.local>
2023-09-19 11:58:18 +0100

    stashing

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -7,17 +7,16 @@ function authJwt() {
         secret,
         algorithms: ['HS256'],
         isRevoked: isRevoked,
+    }).unless({
+        path: [
+            //allow users only GET products
+            // { url: `${api}/products`, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+            `${api}/users/login`,
+            `${api}/users/register`,
+        ],
     })
-    // .unless({
-    //     path: [
-    //         //allow users only GET products
-    //         // { url: `${api}/products`, methods: ['GET', 'OPTIONS'] },
-    //         { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
-    //         { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
-    //         `${api}/users/login`,
-    //         `${api}/users/register`,
-    //     ],
-    // })
 }
 
 async function isRevoked(req, payload, done) {
